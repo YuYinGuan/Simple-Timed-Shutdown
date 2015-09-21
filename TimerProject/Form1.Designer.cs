bb58c2f5c0600cdf8d8f5e4ff60f24fa295a7349
YuYinGuan <intergameviewer@gmail.com>
2015-09-20 21:15:24 -0700

    This is made while learning VS and is in no way perfect. Feel free to improve or use the program.

---
+++ TimerProject/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TimerProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SecondCounter = new System.Windows.Forms.Timer(this.components);
+            this.TimeView = new System.Windows.Forms.Label();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.MinuteLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Begin Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SecondCounter
+            // 
+            this.SecondCounter.Enabled = true;
+            this.SecondCounter.Interval = 1000;
+            this.SecondCounter.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeView
+            // 
+            this.TimeView.Location = new System.Drawing.Point(12, 9);
+            this.TimeView.Name = "TimeView";
+            this.TimeView.Size = new System.Drawing.Size(216, 35);
+            this.TimeView.TabIndex = 1;
+            this.TimeView.Text = "label1";
+            this.TimeView.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.TimeView.Click += new System.EventHandler(this.TimeView_Click);
+            // 
+            // Text1
+            // 
+            this.Text1.AcceptsReturn = true;
+            this.Text1.Location = new System.Drawing.Point(15, 73);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(44, 20);
+            this.Text1.TabIndex = 2;
+            this.Text1.TextChanged += new System.EventHandler(this.Text1_TextChanged);
+            // 
+            // MinuteLabel
+            // 
+            this.MinuteLabel.AutoSize = true;
+            this.MinuteLabel.Location = new System.Drawing.Point(65, 76);
+            this.MinuteLabel.Name = "MinuteLabel";
+            this.MinuteLabel.Size = new System.Drawing.Size(44, 13);
+            this.MinuteLabel.TabIndex = 3;
+            this.MinuteLabel.Text = "Minutes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 105);
+            this.Controls.Add(this.MinuteLabel);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.TimeView);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Timed Shutdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer SecondCounter;
+        private System.Windows.Forms.Label TimeView;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.Label MinuteLabel;
+    }
+}
+
---
+++ TimerProject/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TimeView.Text = "Time Until Shutdown";
+        }
+
+        Boolean TimerStart = false;
+        int TimeCount = 0;
+        int oneMinute = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.TimerStart) 
+            {
+                if (oneMinute > 0)
+                {
+                    oneMinute--;
+                }
+                else
+                {
+                    oneMinute = 60;
+                    TimeCount--;
+                }
+
+                TimeView.Text = TimeCount.ToString() + " Minute " + oneMinute.ToString() + " Second";
+
+                if (oneMinute <= 0 && TimeCount <= 0)
+                {
+                    System.Diagnostics.Process.Start("Shutdown", "-s -t 10");
+                    this.TimerStart = false;
+                }
+                
+            }
+            else
+            {
+                TimeView.Text = "Time Until Shutdown";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.TimerStart) 
+            {
+                this.TimerStart = false;
+                button1.Text = "Start";
+            }
+            else
+            {
+                this.TimerStart = true;
+                button1.Text = "Stop";
+                Int32.TryParse(Text1.Text, out TimeCount);
+                Text1.Clear();
+            }
+        }
+
+        private void TimeView_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Text1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
